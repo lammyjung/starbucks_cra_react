7be02f5f288585057ae6ef3716fb2243f9a2166b
lammyjung <brideoflove@naver.com>
2022-02-08 11:33:41 +0900

    스타벅스 create-react-app 리액트 웹제작

---
+++ src/components/GoTopComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class GoTopComponent extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div class="go-top">
+                    <a href="#" class="go-top-btn">
+                        <img src="./images/btn_prom_up.png" alt="go Top 버튼"/>
+                    </a>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default GoTopComponent;
